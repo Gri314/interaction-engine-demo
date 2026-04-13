75b28e813b61dfd8ab8a7be0d5f7900dc7f1a528
Kdog314 <eshirley2000@gmail.com>
2026-04-12 23:35:46 -0700

    Added some comments to test pushing from the laptop.

--- char/Player/arm_ik.gd
+++ char/Player/arm_ik.gd
@@ -4,5 +4,13 @@ class_name ArmIK extends Node3D
 @onready var Elbow: Node3D = $elbow
 @onready var Hand: Node3D = $hand
 
+"""
+Inverse Kinematics eblow:
+	Unable to find a simple analytic solution that doesn't use the trig functions
+	
+	Updated solution in the google drive. 
+"""
+
 func update_elbow():
+	## get midpoint of shoulder position and hand position
 	pass
